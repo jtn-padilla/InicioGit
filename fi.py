7791182e7a3d3c729cc4afdca0f5fe74bc7af1c7
John <jtn.padilla@gmail.com>
2018-01-29 13:22:51 -0600

    Segundo commit, introducimos fi.py

---
+++ fi.py
@@ -0,0 +1,25 @@
+
+a = 30
+acum = 0
+i = 0
+while i<a:
+	acum = acum + a
+	i += 1
+print(acum)	
+
+acum = 0
+x = 1
+y = 1
+#1 1 2 3 5 8
+print(x)
+print(y)
+for i in range(2,a+1):
+	acum = x + y
+	x = y
+	y = acum 
+	print(acum)
+
+
+
+
+
